37fede737d6f68aea81ab129ab69494e01be3330
fanxiao <tigerfanxiao@gmail.com>
2018-06-06 18:15:36 +0200

    add comments to singleton pattern

--- practice/designpartten/singltonPattern/TestSingleton.java
+++ practice/designpartten/singltonPattern/TestSingleton.java
@@ -5,9 +5,14 @@ import org.junit.Test;
 public class TestSingleton {
 	@Test
 	public void testSingleton() {
+		// initiate the first instance
 		ChocolateBoiler chocolateBoiler01 = ChocolateBoiler.getInstance();
+		// change the status of chocolate boiler by fill method
 		chocolateBoiler01.fill();
+		// intend to get the second instance of Chocolate boiler
 		ChocolateBoiler chocolateBoiler02 = ChocolateBoiler.getInstance();
+		// find the instance status is the same as first instance
+		// so there is only one instance.
 		System.out.println(chocolateBoiler02.isEmpty());
 		System.out.println(chocolateBoiler02.isBoiled());
 	}
