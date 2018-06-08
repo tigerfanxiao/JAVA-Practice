147cf3c25a594c7209d9370604cffd1bdb1f49a5
fanxiao <tigerfanxiao@gmail.com>
2018-06-08 22:46:05 +0200

    change src directory

---
+++ src/practice/basic/sysntax/TestLogger.java
@@ -0,0 +1,20 @@
+package practice.basic.sysntax;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class TestLogger {
+
+	private static Logger logger = Logger.getLogger(TestLogger.class);
+
+	public static void main(String[] args) {
+			
+			PropertyConfigurator.configure("log4j.properties");
+			logger.debug("This is debug");
+			logger.info("This is info");
+			logger.warn("This is warn");
+			logger.error("This is error");
+			logger.fatal("This is fatal");
+			
+		}
+}
